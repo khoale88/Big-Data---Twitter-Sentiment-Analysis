2e27f025f2c5740bd688ec36880684f39a7197f9
khoale88 <lenguyenkhoa1988@gmail.com>
2017-04-30 20:27:24 -0700

    callR function now accepts arbitrary numbers of arguments

--- backend/callR.py
+++ backend/callR.py
@@ -1,4 +1,6 @@
 import subprocess
 
-def callR(script_path, input):
-    subprocess.call(["Rscript", script_path, input])
+def call_rscript(script_path, *args):
+    """call an R script from given script_path with args"""
+
+    subprocess.call(["Rscript", script_path] + [str(arg) for arg in args])
