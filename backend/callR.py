9608e9c666c3e2ffdf71812dd05c7c6e9e66e6f5
khoale88 <lenguyenkhoa1988@gmail.com>
2017-04-30 20:42:57 -0700

    bug fix, add gitignore to backend folder

--- backend/callR.py
+++ backend/callR.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def callR(script_path, *args):
+def call_rscript(script_path, *args):
     """run r script with arbitrary numbers of arguments"""
 
     subprocess.call(["Rscript", script_path] + [str(arg) for arg in args])
