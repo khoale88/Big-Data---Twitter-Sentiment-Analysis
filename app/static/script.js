9169f57c12b1b98ec8d85ae0bbf7c82bba45b792
Azhad <azhadm@atlassian.com>
2017-05-09 04:00:13 -0700

    Adding more UI changes

--- app/static/script.js
+++ app/static/script.js
@@ -15,16 +15,13 @@ $(document).ready(function() {
                 }, null, '\t'),
                 dataType: "json",
                 success: function(data) {
-                    if (data["pics"]) {
-                        console.log(data["pics"])
-                        d = new Date();
-                        $("#locPNG").prop("src", data["pics"]["locPNG"] + "?" + d.getTime())
-                        $("#piePNG").prop("src", data["pics"]["piePNG"] + "?" + d.getTime())
-                        $("#wordCloudPNG").prop("src", data["pics"]["wordCloudPNG"] + "?" + d.getTime())
-                        $("#divImage").show();
-                    } else {
-                        $("#divImage").hide();
-                        // window.location.replace(data["redirect"]);
+
+                },
+                complete: function(xhr, textStatus) {
+                    console.log(xhr.status);
+                    if (xhr.status === 204) {
+                        getWordCloud();
+                        getTrendingTweets();
                     }
                 }
             });
@@ -32,3 +29,81 @@ $(document).ready(function() {
         }
     });
 });
+
+function getWordCloud() {
+    $.ajax({
+        type: 'GET',
+        url: "/wordCloud",
+        dataType: "json",
+        success: function(data) {
+            console.log(xhr.status);
+            if (xhr.status === 202) {
+                setTimeout(getWordCloud, 2000);
+            } else if (xhr.status === 200) {
+                $("#imgWordCloud").prop("src", data["wordCloud"] + "?" + d.getTime());
+                getPieChart();
+            }
+        },
+        complete: function(xhr, textStatus) {
+
+        }
+    });
+}
+
+function getPieChart() {
+    $.ajax({
+        type: 'GET',
+        url: "/pieChart",
+        dataType: "json",
+        success: function(data) {
+            console.log(xhr.status);
+            if (xhr.status === 202) {
+                setTimeout(getPieChart, 2000);
+            } else if (xhr.status === 200) {
+                $("#imgPieChart").prop("src", data["pieChart"] + "?" + d.getTime());
+                getLocationMap();
+            }
+        },
+        complete: function(xhr, textStatus) {
+
+        }
+    });
+}
+
+function getLocationMap() {
+    $.ajax({
+        type: 'GET',
+        url: "/locMap",
+        dataType: "json",
+        success: function(data) {
+            console.log(xhr.status);
+            if (xhr.status === 202) {
+                setTimeout(getLocationMap, 2000);
+            } else if (xhr.status === 200) {
+                $("#imgLocationMap").prop("src", data["locMap"] + "?" + d.getTime());
+            }
+        },
+        complete: function(xhr, textStatus) {
+
+        }
+    });
+}
+
+function getTrendingTweets() {
+    $.ajax({
+        type: 'GET',
+        url: "/topTrends",
+        dataType: "json",
+        success: function(data) {
+            console.log(xhr.status);
+            if (xhr.status === 202) {
+                setTimeout(getWordCloud, 2000);
+            } else if (xhr.status === 200) {
+
+            }
+        },
+        complete: function(xhr, textStatus) {
+
+        }
+    });
+}
